3088a3d39b685a9898c11ec215fb2368d6e64169
KimYoungKen <demenphieudu@gmail.com>
2020-11-24 22:33:38 +0700

    upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace BeautifulGirl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBefore = new System.Windows.Forms.Label();
+            this.textBoxBefore = new System.Windows.Forms.TextBox();
+            this.textBoxAfter = new System.Windows.Forms.TextBox();
+            this.lblAfter = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBefore
+            // 
+            this.lblBefore.AutoSize = true;
+            this.lblBefore.Location = new System.Drawing.Point(18, 16);
+            this.lblBefore.Name = "lblBefore";
+            this.lblBefore.Size = new System.Drawing.Size(38, 13);
+            this.lblBefore.TabIndex = 0;
+            this.lblBefore.Text = "Before";
+            this.lblBefore.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxBefore
+            // 
+            this.textBoxBefore.Location = new System.Drawing.Point(21, 47);
+            this.textBoxBefore.Multiline = true;
+            this.textBoxBefore.Name = "textBoxBefore";
+            this.textBoxBefore.Size = new System.Drawing.Size(228, 198);
+            this.textBoxBefore.TabIndex = 1;
+            this.textBoxBefore.TextChanged += new System.EventHandler(this.textBoxBefore_TextChanged);
+            // 
+            // textBoxAfter
+            // 
+            this.textBoxAfter.Location = new System.Drawing.Point(406, 47);
+            this.textBoxAfter.Multiline = true;
+            this.textBoxAfter.Name = "textBoxAfter";
+            this.textBoxAfter.Size = new System.Drawing.Size(228, 198);
+            this.textBoxAfter.TabIndex = 3;
+            this.textBoxAfter.TextChanged += new System.EventHandler(this.textBoxAfter_TextChanged);
+            // 
+            // lblAfter
+            // 
+            this.lblAfter.AutoSize = true;
+            this.lblAfter.Location = new System.Drawing.Point(403, 16);
+            this.lblAfter.Name = "lblAfter";
+            this.lblAfter.Size = new System.Drawing.Size(29, 13);
+            this.lblAfter.TabIndex = 2;
+            this.lblAfter.Text = "After";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.Location = new System.Drawing.Point(272, 103);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 83);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Beautiful Girl";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.TextChanged += new System.EventHandler(this.textBoxAfter_TextChanged);
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 267);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBoxAfter);
+            this.Controls.Add(this.lblAfter);
+            this.Controls.Add(this.textBoxBefore);
+            this.Controls.Add(this.lblBefore);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Beautiful Girl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBefore;
+        private System.Windows.Forms.TextBox textBoxBefore;
+        private System.Windows.Forms.TextBox textBoxAfter;
+        private System.Windows.Forms.Label lblAfter;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace BeautifulGirl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            _keyWord = GetSetting("Key");
+            if (!string.IsNullOrEmpty(_keyWord))
+            {
+                _listKeyWord = _keyWord.Split(new string[] { " " }, StringSplitOptions.None).ToList();
+                _listKeyWord.ForEach(x => x.ToUpper());
+            }
+        }
+        private static string _textBefore = string.Empty;
+        private static string _keyWord = string.Empty;
+        private List<string> _listKeyWord = new List<string>();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Get string from App.config
+        /// </summary>
+        /// <param name="key">key of node XML</param>
+        /// <returns></returns>
+        private static string GetSetting(string key)
+        {
+            string tempStr = ConfigurationManager.AppSettings[key].Trim();
+            string result = Regex.Replace(tempStr, @"\r\n?|\n|\t|\s", " ");
+            return result;
+        }
+
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_textBefore))
+            {
+                
+                // Split string to List
+                List<string> temp = _textBefore.Split(new string[] { "\r\n" }, StringSplitOptions.None).ToList();
+
+                for (int i = 0; i < temp.Count; i++)
+                {
+                    if (string.IsNullOrEmpty(temp[i])) 
+                    { 
+                        temp.RemoveAt(i);
+                        continue;
+                    }
+                    if (temp[i].IndexOf("sql.AppendLine(\"") == -1)
+                    {
+                        temp[i] = string.Format("sql.AppendLine(\"") + temp[i].ToString().Trim();
+                    }
+                    if (temp[i].LastIndexOf("\");") == -1)
+                    {
+                        temp[i] = temp[i].ToString() + string.Format("\");");
+                    }
+                    temp[i] = temp[i].ToString().Trim();
+                }  
+                
+                ConvertKeyWord(temp);
+                int maxLength = 0;
+                for (int i = 0; i < temp.Count; i++)
+                {
+                    temp[i] = temp[i].Trim();
+                    int idexBegin = temp[i].IndexOf("sql.AppendLine(\"") + string.Format("sql.AppendLine(\"").Length;
+                    int idexEnd = temp[i].IndexOf("\");");
+                    //int idexEnd = temp[i].LastIndexOf("\");") - string.Format("\");").Length;
+                    string textBefore = temp[i].Substring(idexBegin, idexEnd - idexBegin);
+                    // Replace all double space to single space char
+                    string textAfter = Regex.Replace(textBefore, @"\s+", " ");
+                    textAfter = temp[i].Substring(idexBegin, idexEnd - idexBegin).Trim();
+                    
+                    // Find a first word
+                    var tempWordFirst = Regex.Match(textAfter, @"^(\@?\w+)(?=\s|\W)");
+                    // Check whether first word is to belong any keyword
+                    if (!_listKeyWord.Contains(tempWordFirst.ToString().ToUpper()))
+                    {
+                        textAfter = textAfter.Insert(0, "     ");
+                    }
+                    textAfter = textAfter.Insert(0, "  ");
+                    temp[i] = temp[i].Replace(textBefore, textAfter);
+                    var tempMaxLength = temp[i].Length;
+                    if (temp[maxLength].Length < tempMaxLength) maxLength = i;
+                }
+
+                int tempLength = temp[maxLength].Length;
+                for (int i = 0; i < temp.Count; i++)
+                {
+                    //if (i == maxLength) continue;
+                    int reduction = tempLength - temp[i].Length;
+                    // Set space char last the sentence
+                    reduction += 2;
+                    // Define a string builder
+                    StringBuilder tempSpaceChar = new StringBuilder(string.Empty);
+                    for (int x = 0; x < reduction; x++)
+                    {
+                        tempSpaceChar.Append(" ");
+                    }
+
+                    int idexBegin = temp[i].IndexOf("sql.AppendLine(\"") + string.Format("sql.AppendLine(\"").Length;
+                    int idexEnd = temp[i].IndexOf("\");");
+                    string textBefore = temp[i].Substring(idexBegin, idexEnd - idexBegin);
+                    string textAfter = temp[i].Substring(idexBegin, idexEnd - idexBegin) + tempSpaceChar.ToString();
+                    temp[i] = temp[i].Replace(textBefore, textAfter);
+                    temp[i] += "\r\n";
+                }
+
+                // Define stringBuilder output
+                StringBuilder outputString = new StringBuilder(string.Empty);
+                int countOfList = temp.Count;
+                for (int i = 0; i < countOfList; i++)
+                {
+                    outputString.Append(temp[i]);
+                }
+
+                this.textBoxAfter.Text = outputString.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Convert key word of T-SQL to UpperCase
+        /// </summary>
+        /// <param name="listKeyWord"></param>
+        private void ConvertKeyWord(List<string> listKeyWord)
+        {
+            int lengthResource = _listKeyWord.Count;
+            int lengthTurned = listKeyWord.Count;
+
+            for (int i = 0; i < lengthResource; i++)
+            {
+                for (int j = 0; j < lengthTurned; j++)
+                {
+                    // Use regex problem solving
+                    string pattern = string.Format($@"(?<=\s|\{(char)34})(?i)({_listKeyWord[i].Trim().ToUpper()})(?=\s|\W)");
+                    Regex regex = new Regex(pattern);
+                    if (regex.IsMatch(listKeyWord[j]))
+                        listKeyWord[j] = regex.Replace(listKeyWord[j], $"{_listKeyWord[i].Trim().ToUpper()}");
+
+                }
+            }
+        }
+        private void textBoxBefore_TextChanged(object sender, EventArgs e)
+        {
+            TextBox objBefore = (TextBox)sender;
+            _textBefore = objBefore.Text;
+        }
+
+        private void textBoxAfter_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
